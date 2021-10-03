a1da95f4c01dfd13fc43f107fafc4b72b48d9558
YeXinZ <390651407@qq.com>
2021-10-03 15:11:48 +0800

    提交

--- api/index.js
+++ api/index.js
@@ -18,10 +18,69 @@ export const activityList = (data) => {
     data
   })
 }
-
+// 获取活动详情
+export const activityInfo = (id) => {
+  return request({
+    url: '/api/Activity/activityInfo',
+    method: 'GET',
+    data: { id }
+  })
+}
+// 活动报名
+export const activitySignUp = (data) => {
+  return request({
+    url: '/api/Activity/activitySignUp',
+    method: 'POST',
+    data
+  })
+}
+// 获取文章分类
+export const getArticleType = () => {
+  return request({
+    url: '/api/Article/getArticleType',
+    method: 'GET'
+  })
+}
+// 文章详情
+export const getArticleInfo = (id) => {
+  return request({
+    url: '/api/Article/getArticleInfo',
+    method: 'GET',
+    data: { id }
+  })
+}
+// 获取病例
 export const getCase = () => {
   return request({
     url: '/api/Usercase/getCase',
+    method: 'GET'
+  })
+}
+// 获取用药指导
+export const getMedication = () => {
+  return request({
+    url: '/api/Guide/getMedication',
+    method: 'GET'
+  })
+}
+// 获取热门推荐文章
+export const getHotArticle = () => {
+  return request({
+    url: '/api/Article/getHotArticle',
+    method: 'GET'
+  })
+}
+// 获取饮食指导信息
+export const getDiet = () => {
+  return request({
+    url: '/api/Guide/getDiet',
+    method: 'GET'
+  })
+}
+// 获取运动指导
+export const getMotion = () => {
+  return request({
+    url: '/api/Guide/getMotion',
     method: 'GET'
   })
 }
--- app.js
+++ app.js
@@ -14,6 +14,7 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    imgUrl: "http://img.xunshanfang.cn/"
   }
 })

f8240837a9a9eaff7bf1e15075a947ebffd08edd
YexinZhang <yexinzhangyxz@163.com>
2021-11-16 18:07:30 +0800

    提交

--- app.js
+++ app.js
@@ -2,14 +2,36 @@
 App({
   onLaunch() {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // const logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //   }
+    // })
+  },
+  getUserInfo(callback) {
+    // const phone = wx.getStorageSync({
+    //   key: 'phone'
+    // });
+    // if (phone) {
+
+    // } else {
+
+    // }
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success() {
+
+            }
+          })
+        }
       }
     })
   },
